70e698f39416a5cf9d93b8bcd46f19e31df6b9e1
Jean Fritz Duverseau <duverseau.jeanfritz@gmail.com>
2022-04-08 08:11:26 -0400

    change Connexion type String

--- Data/DAO/ApplicationDbContext.cs
+++ Data/DAO/ApplicationDbContext.cs
@@ -26,42 +26,42 @@ namespace Brain.Dev.GStock.Data.DAO
             this.appSettings = appsettings.Value;
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder builder)
-        {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            if (string.IsNullOrWhiteSpace(environment))
-                throw new Exception("Missing environment name from configuration, check your launchSettings.json and try again.");//environment = "Production";//
+        //protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        //{
+        //    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        //    if (string.IsNullOrWhiteSpace(environment))
+        //        throw new Exception("Missing environment name from configuration, check your launchSettings.json and try again.");//environment = "Production";//
 
-            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-                                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                                        .AddJsonFile($"appsettings.{environment.ToLowerInvariant()}.json", optional: true)
-                                        .AddEnvironmentVariables()
-                                        .Build();
+        //    var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+        //                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        //                                .AddJsonFile($"appsettings.{environment.ToLowerInvariant()}.json", optional: true)
+        //                                .AddEnvironmentVariables()
+        //                                .Build();
 
-            if (configuration == null)
-            {
-                logger.LogInformation($"Failed to build configuration [ '{environment}' environment ]");
-                throw new Exception("Failed to build configuration");
-            }
+        //    if (configuration == null)
+        //    {
+        //        logger.LogInformation($"Failed to build configuration [ '{environment}' environment ]");
+        //        throw new Exception("Failed to build configuration");
+        //    }
 
 
-            //var useInMemory = _forceInMemory || (bool.TryParse(Environment.GetEnvironmentVariable("GS_Database__InMemory"), out bool inMemoryConfig) && inMemoryConfig);
-            //logger.LogInformation($"Configuring database context for '{environment}' environment (Use in memory db: useInMemory)");
+        //    //var useInMemory = _forceInMemory || (bool.TryParse(Environment.GetEnvironmentVariable("GS_Database__InMemory"), out bool inMemoryConfig) && inMemoryConfig);
+        //    //logger.LogInformation($"Configuring database context for '{environment}' environment (Use in memory db: useInMemory)");
 
-            //if (useInMemory)
-            //    return;
+        //    //if (useInMemory)
+        //    //    return;
 
-            var connectionString = configuration.GetConnectionString(ConnectionStringName);
-            connectionString = this.connectionStrings.DefaultConnectionString;
-            if (string.IsNullOrWhiteSpace(connectionString))
-            {
-                logger.LogError($"Could not find/read connection string '{ConnectionStringName}', make sure it's properly set in the configuration.");
-                return;
-            }
+        //    var connectionString = configuration.GetConnectionString(ConnectionStringName);
+        //    connectionString = this.connectionStrings.DefaultConnectionString;
+        //    if (string.IsNullOrWhiteSpace(connectionString))
+        //    {
+        //        logger.LogError($"Could not find/read connection string '{ConnectionStringName}', make sure it's properly set in the configuration.");
+        //        return;
+        //    }
 
-            builder.UseMySQL(connectionString, options => options.MigrationsAssembly("api.ccg.rezo509.com"));
-            base.OnConfiguring(builder);
-        }
+        //    builder.UseMySQL(connectionString, options => options.MigrationsAssembly("api.ccg.rezo509.com"));
+        //    base.OnConfiguring(builder);
+        //}
 
         public DbSet<CategoryModel> Categories { get; set; }
     }
